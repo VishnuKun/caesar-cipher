69fad42e550959d07de30d4e2fd49fd6dfd11dd4
Vishnu <vishnukun954AV@protonmail.com>
2023-08-26 09:54:43 +0530

    Added new test for numbers and punctuations

--- spec/caesar_cipher_spec.rb
+++ spec/caesar_cipher_spec.rb
@@ -15,6 +15,9 @@ describe 'caesar_cipher' do
     expect(caesar_cipher('ToMaTo', 10)).to eql('DyWkDy')
   end
   it 'should work with negative shift' do
-    expect(caesar_cipher('potato', -16)).to eql('zydkdy')
+    expect(caesar_cipher('potato', -5)).to eql('kjovoj')
+  end
+  it 'should not mess with strings which have numbers and punctuations in between' do
+    expect(caesar_cipher('123 lord is 4 eva!', 5)).to eql('123 qtwi nx 4 jaf!')
   end
 end
